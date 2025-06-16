c00c11a7dd6a39af0ebc2ae99a9c65447a8ddbbc
pshetty99 <prateek@datamotive.io>
2025-06-16 12:28:21 +0530

    added failure message to recovery job

--- support/resetVM.go
+++ support/resetVM.go
@@ -28,7 +28,7 @@ func main() {
 	queries := []string{
 		fmt.Sprintf(`UPDATE disk_replication_job_infos SET reset_iteration = 0 WHERE vm_name='%s' AND reset_iteration = 1`, *vmName),
 		fmt.Sprintf(`UPDATE vm_replication_job_infos SET reset_iteration = 0 WHERE vm_name='%s' AND reset_iteration = 1`, *vmName),
-		fmt.Sprintf(`UPDATE vm_recovery_job_infos SET status='failed' WHERE vm_name='%s' AND status='completed' ORDER BY id DESC LIMIT 1`, *vmName),
+		fmt.Sprintf(`UPDATE vm_recovery_job_infos SET status='failed',failure_message='Recovery status reset by the user' WHERE vm_name='%s' AND status='completed' ORDER BY id DESC LIMIT 1`, *vmName),
 		fmt.Sprintf(`UPDATE protection_plan_recovery_job_infos SET status='partially-completed' WHERE protection_plan_id=%d ORDER BY id DESC LIMIT 1`, *planID),
 		fmt.Sprintf(`UPDATE virtual_machines SET recovery_status = '' WHERE name='%s'`, *vmName),
 		fmt.Sprintf(`UPDATE protection_plans SET recovery_status = '' WHERE id=%d`, *planID),
