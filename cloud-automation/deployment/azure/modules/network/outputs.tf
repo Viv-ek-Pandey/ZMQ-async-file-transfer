cf61edab5ff4b7d9c5e978728bf1fe3fc7127ec6
Vivek <vying.vicky@gmail.com>
2025-10-03 19:31:14 +0530

    Revert "Added Terraform script for all clouds"
    This reverts commit aa87de938307c9b9b9ce8633ac60eefff43ba531.

--- cloud-automation/deployment/azure/modules/network/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repl_network_interfaces" {
-  value = azurerm_network_interface.repl[*].id
-}
-output "prep_network_interfaces" {
-  value = azurerm_network_interface.prep[*].id
-}
-output "mgmt_network_interfaces" {
-  value = azurerm_network_interface.mgmt[*].id
-}
